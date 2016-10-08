56dc08aa09b978796d3c3647173ac06b2a466c0a
snowpool <snowpoollovely@gmail.com>
2016-10-08 23:08:11 +0900

    gitignore test

---
+++ app/models/picture_store.rb
@@ -0,0 +1,7 @@
+class PictureStore < ActiveRecord::Base
+  mount_uploader :picture, PictureUploader
+  validates :picture,
+          :file_size => {
+              :maximum => 1.megabytes.to_i
+          }
+end
